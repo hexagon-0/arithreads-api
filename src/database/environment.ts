38959d45bdc6c1fd97fc9cba119104f9e9aadda2
Hexagon-0 <lucasosouza66@gmail.com>
2024-07-30 06:30:51 -0300

    Add Knex environment for production and pg package

--- src/database/environment.ts
+++ src/database/environment.ts
@@ -1,15 +1,17 @@
 import { Knex } from 'knex';
 import path from 'path';
 
+const migrations = {
+  directory: path.resolve(__dirname, 'migrations'),
+};
+
 export const development: Knex.Config = {
   client: 'sqlite3',
   useNullAsDefault: true,
   connection: {
     filename: path.resolve(__dirname, '..', '..', 'database.db'),
   },
-  migrations: {
-    directory: path.resolve(__dirname, 'migrations'),
-  },
+  migrations,
   pool: {
     afterCreate: function (conn: any, done: Function) {
       conn.run('PRAGMA foreign_keys = ON');
@@ -24,5 +26,13 @@ export const test: Knex.Config = {
 };
 
 export const production: Knex.Config = {
-  ...development,
+  client: 'pg',
+  connection: process.env.DB_CONNECTION_STRING || {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT || 5432),
+  },
+  migrations,
 };
